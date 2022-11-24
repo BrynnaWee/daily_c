2f533baae3eaaf4e098b5ce113da52184d21ebb5
Brynna.we <Brynna.we@gmail.com>
2022-11-24 16:18:29 +0900

    11-24 update

---
+++ 8-test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int zipCode = 3299;
+    printf("%d\n", zipCode); //출력 결과 : 3299
+    printf("%06d\n", zipCode); //출력 결과 : 003299
+
+    return 0;
+}
+
---
+++ 9-1-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char c1 = 97; // a의 ASCII 코드값 97 저장
+    char c2 = 98; // b의 ASCII 코드값 98 저장
+
+    printf("%c, %d\n", c1, c1); // 출력 결과 : a, 97
+    printf("%c, %d\n", c2, c2); // 출력 결과 : b, 98
+
+    return 0;
+}
