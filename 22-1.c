16af1367c6166a43475018854a97c1c192e6b9e1
Brynna.we <Brynna.we@gmail.com>
2022-11-29 17:41:49 +0900

    11-29 update2(구조정리)

---
+++ 22-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h> //bool, true, false가 정의된 헤더 파일이다.
+
+int main()
+{
+    bool b1 = false;
+
+    if(b1)
+        printf("참\n");
+    else
+        printf("거짓\n");
+
+    return 0;
+}
